22a48e57c34e0634ea1063670c7f1fb03fd5a7e0
3ue4 <3ue4@fresnou.org>
2022-01-20 08:40:11 -0800

    initial commit two

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 const employeeController = require('../controllers/employeeController')
 
 /* GET home page. */
-router.get('/', employeeController.displayEmployees)
-router.get('/add', employeeController.renderAddEmployeeForm)
-router.post('/add'. employeeController.addEmployee)
+router.get('/', employeeController.displayEmployees);
+router.get('/add', employeeController.renderAddEmployeeForm);
+router.post('/add', employeeController.addEmployee);
+router.get('/update/:id', employeeController.renderUpdateForm);
+router.post('/update/:id', employeeController.updateEmployee);
+router.get('/delete/:id', employeeController.deleteEmployee);
 module.exports = router;
